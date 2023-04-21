e60317e66f86335c7bf3d3c7dfbfd1ae59b9c4a0
Иван Кухаренко <idkukharenko@edu.hse.ru>
2023-04-22 01:04:05 +0300

    добавлен докер и некоторая реализация запросов

--- main.py
+++ main.py
@@ -4,33 +4,49 @@ import time
 
 app = FastAPI()
 
+pid = -1
+
 @app.get("/api/docs")
 def getDocs():
     pass
 
-@app.post("/api/start_pn")
+@app.post("/api/start_printpi")
 def start():
-    pid = os.fork()
-    if pid:
-        print('Child process id:', pid)
-        pi = ""
-        with open("pi.txt", "r") as file:
-            pi = file.readline()
-        for i in range(0, len(pi) - 6, 6):
-            print(pi[i : i + 6], end = '')
-            time.sleep(1)
+    if pid == -1:
+        pid = os.fork()
+        if pid:
+            print('Child process id:', pid)
+            pi = ""
+            with open("pi.txt", "r") as file:
+                pi = file.readline()
+            for i in range(0, len(pi) - 6, 6):
+                print(pi[i : i + 6], end = '')
+                time.sleep(1)
+            pid = -1
+        else:
+            print("Can't run a process")
     else:
-        print('Can\'t run a process')
+        print("Can't run a process")
 
 
-@app.post("/api/stop_pn")
+@app.post("/api/stop_printpi")
 def stop():
-    pass
+    if pid != -1:
+        os.kill(pid, 0)
+    else:
+        print("Process has already stopped")
 
-@app.get("/api/pn")
+
+@app.get("/api/status_printpi")
 def status():
-    pass
+    if pid == -1:
+        return "Not launched"
+    else:
+        return "Launched"
 
 @app.get("/api/pn/result")
 def result():
-    pass
+    if pid != -1:
+        pass
+    else:
+        pass
