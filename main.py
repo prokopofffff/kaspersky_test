8eee29d79c69d307974d942f5be1a22a3dfe46a8
Иван Кухаренко <idkukharenko@edu.hse.ru>
2023-04-21 22:23:46 +0300

    в качестве процесса выводим число пи по 6 символов каждую секунду

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import os
+import time
 
 app = FastAPI()
 
@@ -7,11 +9,22 @@ def getDocs():
     pass
 
 @app.post("/api/start_pn")
-async def start():
-    pass
+def start():
+    pid = os.fork()
+    if pid:
+        print('Child process id:', pid)
+        pi = ""
+        with open("pi.txt", "r") as file:
+            pi = file.readline()
+        for i in range(0, len(pi) - 6, 6):
+            print(pi[i : i + 6], end = '')
+            time.sleep(1)
+    else:
+        print('Can\'t run a process')
+
 
 @app.post("/api/stop_pn")
-async def stop():
+def stop():
     pass
 
 @app.get("/api/pn")
