03157a760334a3e0fd843b1febbed4b7072c15c4
Иван Кухаренко <idkukharenko@edu.hse.ru>
2023-04-22 13:39:18 +0300

    tests

--- app/process.py
+++ app/process.py
@@ -1,6 +1,6 @@
 import time
 
-with open('../kaspersky_test/pi.txt', 'r') as file:
+with open('../kaspersky_test/ap/pi.txt', 'r') as file:
     pi = file.readline()
     for i in range(0, len(pi) - 6, 6):
         print(pi[i : i + 6], end = '')
